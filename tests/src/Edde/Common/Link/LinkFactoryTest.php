f210beeb425ae1c588b164703be609313ab95eb4
Marek Hanzal <marek-hanzal@edde-framework.org>
2016-08-04 22:09:31 +0200

    - another few declare strict...

--- tests/src/Edde/Common/Link/LinkFactoryTest.php
+++ tests/src/Edde/Common/Link/LinkFactoryTest.php
@@ -1,13 +1,24 @@
 <?php
+	declare(strict_types = 1);
+
 	namespace Edde\Common\Link;
 
+	use Edde\Api\Link\ILinkFactory;
 	use Edde\Api\Url\IUrl;
 	use phpunit\framework\TestCase;
 
 	class LinkFactoryTest extends TestCase {
+		/**
+		 * @var ILinkFactory
+		 */
+		protected $linkFactory;
+
 		public function testCommon() {
-			$linkFactory = new LinkFactory();
-			self::assertInstanceOf(IUrl::class, $uri = $linkFactory->linkTo($this, 'testCommon'));
+			self::assertInstanceOf(IUrl::class, $uri = $this->linkFactory->linkTo($this, 'testCommon'));
 			self::assertEquals('/edde/common/link/link-factory-test/test-common', $uri->getPath());
+		}
+
+		protected function setUp() {
+			$this->linkFactory = new LinkFactory();
 		}
 	}
