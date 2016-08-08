6e92666c4268d2ea71e0bd7c7ff69891838e3708
Marek Hanzal <marek-hanzal@edde-framework.org>
2016-08-08 16:47:14 +0200

    - edde view is not using kube by default to be a bit more flexible; hard dependency is only jQuery

--- src/Edde/Ext/Html/EddeViewControl.php
+++ src/Edde/Ext/Html/EddeViewControl.php
@@ -11,10 +11,8 @@
 	 */
 	class EddeViewControl extends ViewControl {
 		protected function prepare() {
-			parent::prepare();
-			$this->addStyleSheet(new FileResource(__DIR__ . '/assets/css/kube.css'));
-			$this->addStyleSheet(new FileResource(__DIR__ . '/assets/css/edde-framework.css'));
 			$this->addJavaScript(new FileResource(__DIR__ . '/assets/js/jquery-3.1.0.js'));
 			$this->addJavaScript(new FileResource(__DIR__ . '/assets/js/edde-framework.js'));
+			$this->addStyleSheet(new FileResource(__DIR__ . '/assets/css/edde-framework.css'));
 		}
 	}
