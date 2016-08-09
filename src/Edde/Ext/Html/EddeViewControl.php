87eb9af18a37b4f13c92fae160f747817a1a9064
Marek Hanzal <marek-hanzal@edde-framework.org>
2016-08-09 07:02:41 +0200

    - missed parent call

--- src/Edde/Ext/Html/EddeViewControl.php
+++ src/Edde/Ext/Html/EddeViewControl.php
@@ -11,6 +11,7 @@
 	 */
 	class EddeViewControl extends ViewControl {
 		protected function prepare() {
+			parent::prepare();
 			$this->addJavaScript(new FileResource(__DIR__ . '/assets/js/jquery-3.1.0.js'));
 			$this->addJavaScript(new FileResource(__DIR__ . '/assets/js/edde-framework.js'));
 			$this->addStyleSheet(new FileResource(__DIR__ . '/assets/css/edde-framework.css'));
