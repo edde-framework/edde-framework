f163236c14d6c557051133c4c8269233907f6926
Marek Hanzal <marek-hanzal@edde-framework.org>
2016-09-03 14:52:05 +0200

    - action formally needs () as other "function calls"

--- src/Edde/Common/Html/MacroSet.php
+++ src/Edde/Common/Html/MacroSet.php
@@ -237,7 +237,10 @@
 					if (isset($attributeList['action']) === false) {
 						throw new MacroException(sprintf('Missing mandatory attribute "action" in [%s].', $root->getPath()));
 					}
-					$action = $root->getAttribute('action');
+					if (strrpos($action = $root->getAttribute('action'), '()', 0) === false) {
+						throw new MacroException(sprintf('Action [%s] attribute needs to have () at the end.', $action));
+					}
+					$action = str_replace('()', '', $action);
 					unset($attributeList['action']);
 					$destination->write(sprintf("\t\t\t\$control->setAction([\$this->proxy, %s]);\n", $compiler->value($action)));
 					$this->writeAttributeList($attributeList, $destination);
