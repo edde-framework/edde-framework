25f977be853568ec9c59229e84d1c1c6c14821f3
Marek Hanzal <marek-hanzal@edde-framework.org>
2016-09-07 07:59:41 +0200

    - fixed first loop test

--- src/Edde/Common/Html/Macro/LoopMacro.php
+++ src/Edde/Common/Html/Macro/LoopMacro.php
@@ -17,7 +17,6 @@
 		public function __construct() {
 			parent::__construct([
 				'loop',
-				'm:loop',
 			]);
 			$this->loopStack = new \SplStack();
 		}
@@ -46,35 +45,20 @@
 			]);
 			switch ($macro->getName()) {
 				case 'loop':
-					$this->checkAttribute($macro, $element, 'src');
+					$source = $macro->getAttribute('src', $macro->getValue());
 					$destination->write(sprintf("\t\t\t\t/** macro: %s */\n", $macro->getPath()));
-					$destination->write(sprintf("\t\t\t\$controlList[%s] = function(%s \$root) use(&\$controlList, &\$stash) {\n", $compiler->delimite($element->getMeta('control')), IControl::class));
+					$destination->write(sprintf("\t\t\t\$controlList[%s] = function(%s \$root) use(&\$controlList, &\$stash) {\n", $compiler->delimite($macro->getMeta('control')), IControl::class));
 					$destination->write("\t\t\t\t\$control = \$root;\n");
-					$destination->write(sprintf("\t\t\t\tforeach(%s as \$%s => \$%s) {\n", $compiler->delimite($element->getAttribute('src')), $key, $value));
+					$destination->write(sprintf("\t\t\t\tforeach(%s as \$%s => \$%s) {\n", $compiler->delimite($source), $key, $value));
 					$destination->write(sprintf("\t\t\t\t\t\$stash[%s] = \$%s;\n", $compiler->delimite($key), $key));
 					$destination->write(sprintf("\t\t\t\t\t\$stash[%s] = \$%s;\n", $compiler->delimite($value), $value));
-					$destination->write(sprintf("\t\t\t\t/** %s */\n", $element->getPath()));
-					foreach ($element->getNodeList() as $node) {
-						$destination->write(sprintf("\t\t\t\tisset(\$controlList[%s]) ? \$controlList[%s](\$control) : null;\n", $id = $compiler->delimite($node->getMeta('control')), $id));
+					$destination->write(sprintf("\t\t\t/** %s (%s) */\n", $macro->getPath(), $element->getPath()));
+					foreach ($macro->getNodeList() as $node) {
+						$destination->write(sprintf("\t\t\t\t\$controlList[%s](\$control);\n", $compiler->delimite($node->getMeta('control'))));
 					}
 					$destination->write("\t\t\t\t}\n");
 					$destination->write("\t\t\t};\n");
-					$this->element($element, $compiler);
-					$this->loopStack->pop();
-					break;
-				case 'm:loop':
-					$this->checkValue($macro, $element);
-					$destination->write(sprintf("\t\t\t\t/** inline-macro: %s for %s */\n", $macro->getPath(), $element->getPath()));
-					$destination->write(sprintf("\t\t\t\$controlList[%s] = function(%s \$root) use(&\$controlList, &\$stash) {\n", $compiler->delimite(sha1(random_bytes(64))), IControl::class));
-					$destination->write("\t\t\t\t\$control = \$root;\n");
-					$destination->write(sprintf("\t\t\t\tforeach(%s as \$%s => \$%s) {\n", $compiler->delimite($macro->getValue()), $key, $value));
-					$destination->write(sprintf("\t\t\t\t\t\$stash[%s] = \$%s;\n", $compiler->delimite($key), $key));
-					$destination->write(sprintf("\t\t\t\t\t\$stash[%s] = \$%s;\n", $compiler->delimite($value), $value));
-					$destination->write(sprintf("\t\t\t\t/** %s */\n", $element->getPath()));
-					$destination->write(sprintf("\t\t\t\tisset(\$controlList[%s]) ? \$controlList[%s](\$control) : null;\n", $id = $compiler->delimite($element->getMeta('control')), $id));
-					$destination->write("\t\t\t\t}\n");
-					$destination->write("\t\t\t};\n");
-					$compiler->macro($element, $element);
+					$this->element($macro, $compiler);
 					$this->loopStack->pop();
 					break;
 			}
