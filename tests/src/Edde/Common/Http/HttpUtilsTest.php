b11afad4db09ad4e9438af4edbb431ce5567708e
Marek Hanzal <marek-hanzal@edde-framework.org>
2016-09-11 14:50:55 +0200

    - initial simple implementation of an accept header parser

---
+++ tests/src/Edde/Common/Http/HttpUtilsTest.php
@@ -0,0 +1,18 @@
+<?php
+	declare(strict_types = 1);
+
+	namespace Edde\Common\Http;
+
+	use phpunit\framework\TestCase;
+
+	class HttpUtilsTest extends TestCase {
+		public function testAcceptHeader() {
+			self::assertEquals([
+				'text/html',
+				'application/xhtml+xml',
+				'image/webp',
+				'application/xml',
+				'*/*',
+			], HttpUtils::accept('text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'));
+		}
+	}
