3f9c237901f5202b66eaabfc26e5c6e717370a1c
Marek Hanzal <marek-hanzal@edde-framework.org>
2016-09-11 16:27:10 +0200

    - initial integration of HttpUtils into a HeaderList

--- src/Edde/Api/Http/IHeaderList.php
+++ src/Edde/Api/Http/IHeaderList.php
@@ -9,4 +9,62 @@
 	 * Explicit interface for http header list; missing array access interface is intentional.
 	 */
 	interface IHeaderList extends IList {
+		/**
+		 * return user agent
+		 *
+		 * @param string $default
+		 *
+		 * @return string
+		 */
+		public function getUserAgent(string $default = ''): string;
+
+		/**
+		 * return prioritized accept mime
+		 *
+		 * @return string
+		 */
+		public function getAccept(): string;
+
+		/**
+		 * return an ordered array of accepted mime types
+		 *
+		 * @return array
+		 */
+		public function getAcceptList(): array;
+
+		/**
+		 * return propritized language from the current header set or default one
+		 *
+		 * @param string $default
+		 *
+		 * @return string
+		 */
+		public function getAcceptLanguage(string $default): string;
+
+		/**
+		 * return an ordered array of accept-lang
+		 *
+		 * @param string $default
+		 *
+		 * @return array
+		 */
+		public function getAccpetLanguageList(string $default): array;
+
+		/**
+		 * return preffered charset
+		 *
+		 * @param string $default
+		 *
+		 * @return string
+		 */
+		public function getAcceptCharset(string $default): string;
+
+		/**
+		 * return an odered list of accept charsets
+		 *
+		 * @param string $default
+		 *
+		 * @return array
+		 */
+		public function getAcceptCharsetList(string $default): array;
 	}
