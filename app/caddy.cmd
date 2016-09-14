ab8ab70ba3ab9d1cfade575faa8af7fedde3ad94
Marek Hanzal <marek-hanzal@edde-framework.org>
2016-09-14 21:38:23 +0200

    - removed "demo app" folder - edde is now a bit more lightier

--- app/caddy.cmd
+++
@@ -1,5 +0,0 @@
-.php\php "%~dp0..\bin\composer.phar" update
-start /b .php\php-cgi.exe -b 127.0.0.1:9080
-start /b .php\memcached.exe
-start /b bin\caddy.exe
-rem start /b explorer http://127.0.0.1
--- app/src/loader.php
+++
@@ -1,6 +0,0 @@
-<?php
-	declare(strict_types = 1);
-
-	use Edde\Common\Autoloader;
-
-	Autoloader::register(__NAMESPACE__, __DIR__, false);
