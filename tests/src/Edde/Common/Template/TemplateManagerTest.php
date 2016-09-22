6b0d53c46c83271ca03729099baaeeb7798c94e3
Marek Hanzal <marek-hanzal@edde-framework.org>
2016-09-22 16:06:52 +0200

    - fixed template manager test

--- tests/src/Edde/Common/Template/TemplateManagerTest.php
+++ tests/src/Edde/Common/Template/TemplateManagerTest.php
@@ -26,7 +26,6 @@
 	use Edde\Common\File\RootDirectory;
 	use Edde\Common\File\TempDirectory;
 	use Edde\Common\Html\AbstractHtmlTemplate;
-	use Edde\Common\Html\Tag\DivControl;
 	use Edde\Common\Http\HostUrl;
 	use Edde\Common\Link\ControlLinkGenerator;
 	use Edde\Common\Link\LinkFactory;
@@ -37,6 +36,8 @@
 	use Edde\Ext\Converter\XmlConverter;
 	use Edde\Ext\Template\DefaultMacroSet;
 	use phpunit\framework\TestCase;
+
+	require_once(__DIR__ . '/assets.php');
 
 	class TemplateManagerTest extends TestCase {
 		/**
@@ -61,7 +62,7 @@
 				__DIR__ . '/template/complex/to-be-used.xml',
 			]);
 			$template = AbstractHtmlTemplate::template($file, $this->container);
-			$template->snippet($div = new DivControl());
+			$template->snippet($div = new \SomeCoolControl());
 			$div->addClass('root');
 			$div->dirty();
 			self::assertEquals('<div class="root">
@@ -87,7 +88,7 @@
 	</div>
 	<div class="poo-class">poo</div>
 	<div class="used-div-here"></div>
-	<div data-class="Edde.Common.Html.Tag.ButtonControl" class="button" data-action="http://localhost/foo/bar?a=1&control=Edde%5CCommon%5CHtml%5CTag%5CDivControl&action=action-on-the-root"></div>
+	<div data-class="Edde.Common.Html.Tag.ButtonControl" class="button" data-action="http://localhost/foo/bar?a=1&control=SomeCoolControl&action=action-on-the-root"></div>
 	<div data-class="Edde.Common.Html.Tag.ButtonControl" class="button" data-action="http://localhost/foo/bar?a=1&control=Edde%5CCommon%5CHtml%5CTag%5CButtonControl&action=%40action-on-the-current-contol"></div>
 	<div data-class="Edde.Common.Html.Tag.ButtonControl" class="button just-useless-button-here"></div>
 	<div class="edde-placeholder" id="foo"></div>
