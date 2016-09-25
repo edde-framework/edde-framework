8e5baf80d47ef8d973f2e8efa829e63272ea5206
Marek Hanzal <marek-hanzal@edde-framework.org>
2016-09-25 13:07:52 +0200

    - fixed AbstractHtmlMacro

--- src/Edde/Common/Html/Macro/AbstractHtmlMacro.php
+++ src/Edde/Common/Html/Macro/AbstractHtmlMacro.php
@@ -4,25 +4,45 @@
 	namespace Edde\Common\Html\Macro;
 
 	use Edde\Api\File\IFile;
+	use Edde\Api\Node\INode;
+	use Edde\Api\Template\ICompiler;
 	use Edde\Common\Template\AbstractMacro;
 
 	/**
 	 * Abstract class for all html package based macros.
 	 */
 	abstract class AbstractHtmlMacro extends AbstractMacro {
-		protected function writeTextValue() {
-			if (($value = $this->extract($this->macro, 'value', $this->macro->isLeaf() ? $this->macro->getValue() : null, false)) !== null) {
-				$this->write(sprintf('$control->setText(%s);', ($helper = $this->compiler->helper($value)) ? $helper : var_export($value, true)), 5);
+		/**
+		 * write (export) text value from macro node
+		 *
+		 * @param INode $macro
+		 * @param ICompiler $compiler
+		 */
+		protected function writeTextValue(INode $macro, ICompiler $compiler) {
+			if (($value = $this->extract($macro, 'value', $macro->isLeaf() ? $macro->getValue() : null, false)) !== null) {
+				$this->write($compiler, sprintf('$control->setText(%s);', ($helper = $compiler->helper($value)) ? $helper : var_export($value, true)), 5);
 			}
 		}
 
-		protected function write(string $write, int $indents = null) {
+		/**
+		 * shortcut for file write
+		 *
+		 * @param ICompiler $compiler
+		 * @param string $write
+		 * @param int|null $indents
+		 */
+		protected function write(ICompiler $compiler, string $write, int $indents = null) {
 			/** @var $file IFile */
-			$file = $this->compiler->getVariable('file');
+			$file = $compiler->getVariable('file');
 			$file->write(($indents ? str_repeat("\t", $indents) : '') . $write . "\n");
 		}
 
-		protected function writeAttributeList() {
+		/**
+		 * export attribute list
+		 *
+		 * @param ICompiler $compiler
+		 */
+		protected function writeAttributeList(ICompiler $compiler) {
 			$attributeList = $this->getAttributeList(function ($value) {
 				return var_export($value, true);
 			});
@@ -31,13 +51,19 @@
 				foreach ($attributeList as $k => $v) {
 					$attributes[] = var_export($k, true) . ' => ' . $v;
 				}
-				$this->write(sprintf('$control->setAttributeList([%s]);', implode(', ', $attributes)), 5);
+				$this->write($compiler, sprintf('$control->setAttributeList([%s]);', implode(', ', $attributes)), 5);
 			}
 		}
 
-		protected function compile() {
-			foreach ($this->macro->getNodeList() as $node) {
-				$this->compiler->runtimeMacro($node);
+		/**
+		 * compile child nodes of the given macro
+		 *
+		 * @param INode $macro
+		 * @param ICompiler $compiler
+		 */
+		protected function compile(INode $macro, ICompiler $compiler) {
+			foreach ($macro->getNodeList() as $node) {
+				$compiler->runtimeMacro($node);
 			}
 		}
 	}
