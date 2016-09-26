2e605b6099158ee95ec145aa85afa7d813c49f86
Marek Hanzal <marek-hanzal@edde-framework.org>
2016-09-26 22:19:53 +0200

    - hotfix

--- src/Edde/Common/Html/Helper/MethodHelper.php
+++ src/Edde/Common/Html/Helper/MethodHelper.php
@@ -28,7 +28,7 @@
 					'@' => '$stack->top()',
 					':' => '$control->getRoot()',
 				];
-				return sprintf('%s->%s()', $control[$match['type']], StringUtils::camelize($match['method'], null, true));
+				return sprintf('%s->%s($stack->top())', $control[$match['type']], StringUtils::camelize($match['method'], null, true));
 			} else if ($match = StringUtils::match($value, '~^(?<class>(\\\\[a-zA-Z0-9_]+)+)::(?<method>[a-zA-Z_]+)\(\)$~', true, true)) {
 				return sprintf('$this->container->create(%s)->%s($stack->top())', var_export($match['class'], true), $match['method']);
 			}
