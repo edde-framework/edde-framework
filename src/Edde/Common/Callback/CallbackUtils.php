10444cfac4ef8781224f6b1d80d5011053248c26
Marek Hanzal <marek-hanzal@edde-framework.org>
2016-09-27 21:40:39 +0200

    - typehint

--- src/Edde/Common/Callback/CallbackUtils.php
+++ src/Edde/Common/Callback/CallbackUtils.php
@@ -11,6 +11,9 @@
 	use ReflectionFunction;
 	use ReflectionMethod;
 
+	/**
+	 * Useful set of methods around callable reflections.
+	 */
 	class CallbackUtils extends AbstractObject {
 		/**
 		 * @param callable|string $callback
@@ -27,7 +30,7 @@
 		}
 
 		/**
-		 * @param string $callback
+		 * @param string|array $callback
 		 *
 		 * @return ReflectionFunction|ReflectionMethod
 		 */
