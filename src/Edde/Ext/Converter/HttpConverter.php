b7f8ee7a5cdebf6e4ad221b1106307049f26075d
Marek Hanzal <marek-hanzal@edde-framework.org>
2016-09-30 15:34:38 +0200

    - text plain conversion without echoing

--- src/Edde/Ext/Converter/HttpConverter.php
+++ src/Edde/Ext/Converter/HttpConverter.php
@@ -43,6 +43,11 @@
 					}
 					echo $source;
 					return null;
+				case 'string':
+					if (is_callable($source)) {
+						return $source();
+					}
+					return $source;
 			}
 			$this->exception($target);
 		}
