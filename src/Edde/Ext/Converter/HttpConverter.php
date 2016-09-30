b22b9d85d059f8db82f2a143b1b51b5d6183d27d
Marek Hanzal <marek-hanzal@edde-framework.org>
2016-09-30 15:30:49 +0200

    - added plain to plain convertion support

--- src/Edde/Ext/Converter/HttpConverter.php
+++ src/Edde/Ext/Converter/HttpConverter.php
@@ -3,6 +3,7 @@
 
 	namespace Edde\Ext\Converter;
 
+	use Edde\Api\Converter\ConverterException;
 	use Edde\Api\Http\LazyHttpResponseTrait;
 	use Edde\Common\Converter\AbstractConverter;
 
@@ -12,6 +13,9 @@
 	class HttpConverter extends AbstractConverter {
 		use LazyHttpResponseTrait;
 
+		/**
+		 * It is so cold outside I saw a politician with his hands in his own pockets.
+		 */
 		public function __construct() {
 			parent::__construct([
 				'text/plain',
@@ -20,13 +24,19 @@
 			]);
 		}
 
+		/**
+		 * @inheritdoc
+		 * @throws ConverterException
+		 */
 		public function convert($source, string $target) {
 			if (is_callable($source) === false && is_string($source) === false) {
 				$this->unsupported($source, $target);
 			}
 			switch ($target) {
+				/** @noinspection PhpMissingBreakStatementInspection */
 				case 'http+text/plain':
 					$this->httpResponse->send();
+				case 'text/plain':
 					if (is_callable($source)) {
 						$source();
 						return null;
--- src/Edde/Framework.php
+++ src/Edde/Framework.php
@@ -24,7 +24,7 @@
 		 * @return string
 		 */
 		public function getVersion() {
-			return '2.2.410.0';
+			return '2.2.411.0';
 		}
 
 		/**
