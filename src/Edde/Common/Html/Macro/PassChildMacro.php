664588e92e488b24370a43e0f58f2057cd986971
Marek Hanzal <marek-hanzal@edde-framework.org>
2016-10-07 11:03:41 +0200

    - pass macro seems to be working - pass-child is broken

--- src/Edde/Common/Html/Macro/PassChildMacro.php
+++ src/Edde/Common/Html/Macro/PassChildMacro.php
@@ -3,14 +3,16 @@
 
 	namespace Edde\Common\Html\Macro;
 
+	use Edde\Api\Crypt\LazyCryptEngineTrait;
 	use Edde\Api\Node\INode;
 	use Edde\Api\Template\ICompiler;
-	use Edde\Common\Node\Node;
 
 	/**
 	 * Shortcut for multiple nodes injection into some method (variable pass makes no sense).
 	 */
 	class PassChildMacro extends AbstractHtmlMacro {
+		use LazyCryptEngineTrait;
+
 		/**
 		 * Profanity is the one language that all programmers know best.
 		 */
@@ -30,18 +32,35 @@
 		/**
 		 * @inheritdoc
 		 */
-		public function compile(INode $macro, ICompiler $compiler) {
-			$target = $this->extract($macro, 'target');
-			foreach ($macro->getNodeList() as $node) {
-				/** @var $nodeList INode[] */
-				$nodeList = [$node];
-				while ($node->getMeta('root', false) && $node->isLeaf() === false) {
-					$nodeList = $node->getNodeList();
-					$node = $node->getNodeList()[0];
-				}
-				foreach ($nodeList as $child) {
-					$child->prepend(new Node('pass', null, ['target' => $target]));
-				}
-			}
+		public function macro(INode $macro, ICompiler $compiler) {
+			parent::macro($macro, $compiler);
+//			$target = $this->attribute($macro, $compiler, 'target', false);
+//			$func = substr($target, -2) === '()';
+//			$target = str_replace('()', '', $target);
+//			$type = $target[0];
+//			$target = StringUtils::camelize(substr($target, 1), null, true);
+//			$write = sprintf('%s->%s($stack->top());', $this->reference($macro, $type), $target);
+//			if ($func === false) {
+//				$write = sprintf('%s::setProperty(%s, %s, $stack->top());', ReflectionUtils::class, $this->reference($macro, $type), var_export($target, true));
+//			}
+//			foreach ($macro->getNodeList() as $node) {
+//				if ($node->getMeta('snippet', false)) {
+//					continue;
+//				}
+//				$compiler->macro($node);
+//				$this->write($macro, $compiler, sprintf('%s', $write), 5);
+//			}
+//			$target = $this->extract($macro, 'target');
+//			foreach ($macro->getNodeList() as $node) {
+//				/** @var $nodeList INode[] */
+//				$nodeList = [$node];
+//				while ($node->getMeta('root', false) && $node->isLeaf() === false) {
+//					$nodeList = $node->getNodeList();
+//					$node = $node->getNodeList()[0];
+//				}
+//				foreach ($nodeList as $child) {
+//					$child->prepend(new Node('pass', null, ['target' => $target]));
+//				}
+//			}
 		}
 	}
