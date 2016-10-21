9924462b1c905b63deca03352f4ba09d85a50c27
Marek Hanzal <marek-hanzal@edde-framework.org>
2016-10-21 16:40:10 +0200

    - fixed dependencies

--- src/Edde/Ext/Router/EddeRouter.php
+++ src/Edde/Ext/Router/EddeRouter.php
@@ -3,12 +3,18 @@
 
 	namespace Edde\Ext\Router;
 
+	use Edde\Api\Application\LazyResponseManagerTrait;
+	use Edde\Api\Http\LazyHeaderListTrait;
+	use Edde\Api\Http\LazyHttpResponseTrait;
 	use Edde\Api\Http\LazyRequestUrlTrait;
 	use Edde\Common\Application\Request;
 	use Edde\Common\Router\AbstractRouter;
 	use Edde\Module\EddeControl;
 
 	class EddeRouter extends AbstractRouter {
+		use LazyResponseManagerTrait;
+		use LazyHttpResponseTrait;
+		use LazyHeaderListTrait;
 		use LazyRequestUrlTrait;
 
 		public function createRequest() {
