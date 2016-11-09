cb13f70b0c6115592b1cc589c2b741e02bd2aedf
Marek Hanzal <marek-hanzal@edde-framework.org>
2016-11-09 07:42:42 +0100

    - fixed "turn off the cache" bug

--- src/Edde/Common/Cache/DummyCacheManager.php
+++ src/Edde/Common/Cache/DummyCacheManager.php
@@ -3,10 +3,16 @@
 
 	namespace Edde\Common\Cache;
 
+	use Edde\Api\Cache\ICacheManager;
+	use Edde\Api\Cache\ICacheStorage;
 	use Edde\Ext\Cache\DevNullCacheStorage;
 
 	class DummyCacheManager extends CacheManager {
 		public function __construct() {
 			parent::__construct(new DevNullCacheStorage());
+		}
+
+		public function registerCacheStorage(string $namespace, ICacheStorage $cacheStorage): ICacheManager {
+			return $this;
 		}
 	}
--- src/Edde/Framework.php
+++ src/Edde/Framework.php
@@ -24,7 +24,7 @@
 		 * @return string
 		 */
 		public function getVersion() {
-			return '2.4.77.0';
+			return '2.4.78.0';
 		}
 
 		/**
