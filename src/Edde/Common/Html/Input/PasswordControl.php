ce5e740ca332266829866c64274c94d14651a56c
Marek Hanzal <marek-hanzal@edde-framework.org>
2016-11-18 11:26:17 +0100

    - added value setter also to password input - added set placeholder method

--- src/Edde/Common/Html/Input/PasswordControl.php
+++ src/Edde/Common/Html/Input/PasswordControl.php
@@ -6,12 +6,23 @@
 	use Edde\Common\Html\AbstractHtmlControl;
 
 	class PasswordControl extends AbstractHtmlControl {
+		public function setValue(string $value) {
+			$this->data('value', $value);
+			return $this;
+		}
+
+		public function setPlaceholder(string $value) {
+			$this->setAttribute('placeholder', $value);
+			return $this;
+		}
+
 		protected function prepare() {
 			parent::prepare()
 				->javascript(self::class)
 				->setTag('input', false)
 				->addAttributeList([
 					'type' => 'password',
+					'value' => $this->getData('value', ''),
 				]);
 		}
 	}
