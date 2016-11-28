7c9027efef5fba2cc061ea72263429a7fa7d9e5a
Marek Hanzal <marek-hanzal@edde-framework.org>
2016-11-28 08:17:51 +0100

    - fixed apache config

--- .runtime/.run.cmd
+++ .runtime/.run.cmd
@@ -6,6 +6,9 @@ start /b "" "%~dp0.redis\redis-server.exe" "%~dp0.redis.conf"
 cls
 echo Redis is with us!
 timeout 2 /nobreak > nul
+pushd
+chdir "%~dp0.apache"
 echo ...and we're happily online!
 "%~dp0.apache\bin\httpd.exe"
+popd
 pause
