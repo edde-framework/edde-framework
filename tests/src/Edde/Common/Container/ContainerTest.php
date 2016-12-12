757d7c2b53d69c0f28d6e0674394eb3f01055c37
Marek Hanzal <marek-hanzal@edde-framework.org>
2016-12-12 15:26:08 +0100

    - refactored factories to factory list

--- tests/src/Edde/Common/Container/ContainerTest.php
+++ tests/src/Edde/Common/Container/ContainerTest.php
@@ -15,13 +15,15 @@
 	class ContainerTest extends TestCase {
 		public function testContainer() {
 			$container = new Container();
-			$container->registerFactory(new CallbackFactory(function () {
-				return 'bar';
-			}, 'foo'));
-			$container->registerFactory(new CallbackFactory(function (): ICache {
-				return new Cache(new InMemoryCacheStorage());
-			}));
-			$container->registerFactory(new ClassFactory());
+			$container->registerFactoryList([
+				new CallbackFactory(function () {
+					return 'bar';
+				}, 'foo'),
+				new CallbackFactory(function (): ICache {
+					return new Cache(new InMemoryCacheStorage());
+				}),
+				new ClassFactory(),
+			]);
 			self::assertEquals('bar', $container->create('foo'));
 			self::assertEquals('bar', $container->call(function ($foo) {
 				return $foo;
