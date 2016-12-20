d2f6cdf66c06a03e81c237d45ee3a47bef10cacf
Marek Hanzal <marek-hanzal@edde-framework.org>
2016-12-20 10:45:22 +0100

    - optimized crate loader a bit

--- src/Edde/Common/Crate/CrateLoader.php
+++ src/Edde/Common/Crate/CrateLoader.php
@@ -7,14 +7,16 @@
 	use Edde\Api\Crate\ICrateLoader;
 	use Edde\Api\Crate\LazyCrateGeneratorTrait;
 	use Edde\Api\Schema\LazySchemaManagerTrait;
-	use Edde\Common\AbstractObject;
+	use Edde\Common\Cache\CacheTrait;
+	use Edde\Common\Deffered\AbstractDeffered;
 
 	/**
 	 * Default crate loader implementation.
 	 */
-	class CrateLoader extends AbstractObject implements ICrateLoader, ILazyInject {
+	class CrateLoader extends AbstractDeffered implements ICrateLoader, ILazyInject {
 		use LazySchemaManagerTrait;
 		use LazyCrateGeneratorTrait;
+		use CacheTrait;
 
 		/**
 		 * include the requested class
@@ -24,10 +26,19 @@
 		 * @return bool
 		 */
 		public function __invoke(string $class) {
-			if ($this->schemaManager->hasSchema($class) === false) {
+			$this->use();
+			if (($hasSchema = $this->cache->load($cacheId = ('has-schema/' . $class))) === null) {
+				$this->cache->save($cacheId, $hasSchema = $this->schemaManager->hasSchema($class));
+			}
+			if ($hasSchema === false) {
 				return false;
 			}
 			$this->crateGenerator->generate();
 			return class_exists($class);
+		}
+
+		protected function prepare() {
+			parent::prepare();
+			$this->cache();
 		}
 	}
--- src/Edde/Framework.php
+++ src/Edde/Framework.php
@@ -24,7 +24,7 @@
 		 * @return string
 		 */
 		public function getVersion() {
-			return '2.5.32.0';
+			return '2.5.34.0';
 		}
 
 		/**
