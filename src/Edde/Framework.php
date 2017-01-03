bd2be7168b6e482fc8e3182fbe2a1136de280185
Marek Hanzal <marek-hanzal@edde-framework.org>
2017-01-03 11:21:05 +0100

    - version bump

--- src/Edde/Framework.php
+++ src/Edde/Framework.php
@@ -24,7 +24,7 @@
 		 * @return string
 		 */
 		public function getVersion() {
-			return '2.5.53.0';
+			return '2.6.0.0';
 		}
 
 		/**
@@ -33,6 +33,6 @@
 		 * @return string
 		 */
 		public function getCodename() {
-			return 'Dummy Doll';
+			return 'The Blueprint';
 		}
 	}
