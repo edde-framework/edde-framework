935ec40781881b978baa7b75c238a04070f113f0
Marek Hanzal <marek-hanzal@edde-framework.org>
2017-01-10 17:45:53 +0100

    - some trials around ldap server

--- src/Edde/Ext/Stream/LdapConnectionHandler.php
+++ src/Edde/Ext/Stream/LdapConnectionHandler.php
@@ -3,7 +3,18 @@
 
 	namespace Edde\Ext\Stream;
 
+	use Edde\Api\Stream\IConnection;
+	use Edde\Api\Stream\IConnectionHandler;
 	use Edde\Common\Stream\AbstractConnectionHandler;
+	use FG\ASN1\Object;
 
 	class LdapConnectionHandler extends AbstractConnectionHandler {
+		public function read(IConnection $connection): IConnectionHandler {
+			$source = $connection->read();
+			$asn = Object::fromBinary($source);
+			// $asn[2]->getContent()[0][0]->getBinaryContent()
+			// MEECAQFgHQIBAwQRY249c2FmZXEsZGM9c2FmZXGABXNhZmVxoB0wGwQZMS4zLjYuMS40LjEuNDIuMi4yNy44LjUuMQ==
+			// MCQCAQJCAKAdMBsEGTEuMy42LjEuNC4xLjQyLjIuMjcuOC41LjE= (1234568)
+			return $this;
+		}
 	}
