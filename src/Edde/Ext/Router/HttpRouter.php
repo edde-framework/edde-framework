6628be259a29188d9a4d4b457bba505cb23cfc76
Marek Hanzal <marek-hanzal@edde-framework.org>
2017-01-16 14:39:42 +0100

    - http router is a bit more lightweight

--- src/Edde/Ext/Router/HttpRouter.php
+++ src/Edde/Ext/Router/HttpRouter.php
@@ -4,12 +4,8 @@
 	namespace Edde\Ext\Router;
 
 	use Edde\Api\Application\LazyResponseManagerTrait;
-	use Edde\Api\Crate\LazyCrateFactoryTrait;
 	use Edde\Api\Http\LazyBodyTrait;
 	use Edde\Api\Http\LazyHeaderListTrait;
-	use Edde\Api\Http\LazyHttpRequestTrait;
-	use Edde\Api\Http\LazyHttpResponseTrait;
-	use Edde\Api\Http\LazyPostListTrait;
 	use Edde\Api\Http\LazyRequestUrlTrait;
 	use Edde\Api\Runtime\LazyRuntimeTrait;
 	use Edde\Common\Application\Request;
@@ -20,15 +16,11 @@
 	 * Simple http router implementation without any additional magic.
 	 */
 	class HttpRouter extends AbstractRouter {
+		use LazyRuntimeTrait;
+		use LazyRequestUrlTrait;
 		use LazyResponseManagerTrait;
 		use LazyBodyTrait;
-		use LazyRequestUrlTrait;
 		use LazyHeaderListTrait;
-		use LazyPostListTrait;
-		use LazyHttpRequestTrait;
-		use LazyHttpResponseTrait;
-		use LazyRuntimeTrait;
-		use LazyCrateFactoryTrait;
 
 		/**
 		 * @inheritdoc
@@ -42,18 +34,6 @@
 				return null;
 			}
 			$this->responseManager->setTarget($this->headerList->getAcceptList());
-			if ($this->httpRequest->isMethod('GET') === false && ($source = ($this->postList->isEmpty() ? $this->body->convert('array') : $this->postList->array())) !== null) {
-				/**
-				 * support for control property filling
-				 */
-				if (isset($source[''])) {
-					$parameterList[null] = $source[''];
-					unset($source['']);
-				}
-				if (empty($source) === false) {
-					$parameterList = array_merge($parameterList, $this->crateFactory->build($source));
-				}
-			}
 			$contentType = $this->headerList->getContentType();
 			$request = new Request($contentType->getMime());
 			if (isset($parameterList['handle'])) {
