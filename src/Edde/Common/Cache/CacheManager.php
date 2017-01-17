93e50558b0b49889f269b385905fc67c381d095d
Marek Hanzal <marek-hanzal@edde-framework.org>
2017-01-17 13:49:56 +0100

    - returned lost cache stuff and updated .gitignore

---
+++ src/Edde/Common/Cache/CacheManager.php
@@ -0,0 +1,9 @@
+<?php
+	declare(strict_types=1);
+
+	namespace Edde\Common\Cache;
+
+	use Edde\Api\Cache\ICacheable;
+
+	class CacheManager extends AbstractCacheManager implements ICacheable {
+	}
--- src/Edde/Framework.php
+++ src/Edde/Framework.php
@@ -24,7 +24,7 @@
 		 * @return string
 		 */
 		public function getVersion() {
-			return '3.0.994.0';
+			return '3.0.999.0';
 		}
 
 		/**
