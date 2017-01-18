463dd042705d09e196b4a4d9f2f37dbbf58945bd
Marek Hanzal <marek-hanzal@edde-framework.org>
2017-01-18 09:53:23 +0100

    - preparing for git submodules

--- src/Edde/Api/Runtime/RuntimeException.php
+++
@@ -1,9 +0,0 @@
-<?php
-	declare(strict_types = 1);
-
-	namespace Edde\Api\Runtime;
-
-	use Edde\Api\EddeException;
-
-	class RuntimeException extends EddeException {
-	}
--- src/Edde/Api/Url/UrlException.php
+++
@@ -1,9 +0,0 @@
-<?php
-	declare(strict_types = 1);
-
-	namespace Edde\Api\Url;
-
-	use Edde\Api\EddeException;
-
-	class UrlException extends EddeException {
-	}
