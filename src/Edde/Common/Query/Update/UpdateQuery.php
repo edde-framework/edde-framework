c764c62501b945e62d94c62f893b3332fab301f9
Marek Hanzal <marek-hanzal@edde-framework.org>
2017-01-24 16:57:25 +0100

    - fixed mini-pidi bug in UpdateQuery

--- src/Edde/Common/Query/Update/UpdateQuery.php
+++ src/Edde/Common/Query/Update/UpdateQuery.php
@@ -1,14 +1,17 @@
 <?php
-	declare(strict_types = 1);
+	declare(strict_types=1);
 
 	namespace Edde\Common\Query\Update;
 
+	use Edde\Api\Config\IConfigurable;
 	use Edde\Api\Schema\ISchema;
+	use Edde\Common\Config\ConfigurableTrait;
 	use Edde\Common\Node\Node;
 	use Edde\Common\Query\AbstractQuery;
 	use Edde\Common\Query\Where\WhereExpressionFragment;
 
-	class UpdateQuery extends AbstractQuery {
+	class UpdateQuery extends AbstractQuery implements IConfigurable {
+		use ConfigurableTrait;
 		/**
 		 * @var ISchema
 		 */
@@ -38,8 +41,8 @@
 			return $this->whereExpressionFragment;
 		}
 
-		protected function prepare() {
-			parent::prepare();
+		protected function handleInit() {
+			parent::handleInit();
 			$this->node = new Node('update-query', $this->schema->getSchemaName());
 			$this->node->addNodeList([
 				$updateNode = new Node('update'),
