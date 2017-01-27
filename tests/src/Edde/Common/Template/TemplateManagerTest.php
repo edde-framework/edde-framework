fbcf84384f7bc0fc4610007ee1cdf8bacf6c697f
Marek Hanzal <marek-hanzal@edde-framework.org>
2017-01-27 19:02:20 +0100

    - preparing a new template engine

---
+++ tests/src/Edde/Common/Template/TemplateManagerTest.php
@@ -0,0 +1,14 @@
+<?php
+	declare(strict_types=1);
+
+	namespace Edde\Common\Template;
+
+	use PHPUnit\Framework\TestCase;
+
+	/**
+	 * @group wip
+	 */
+	class TemplateManagerTest extends TestCase {
+		public function testSimpleTemplate() {
+		}
+	}
