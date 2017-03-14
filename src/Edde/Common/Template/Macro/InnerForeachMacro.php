bcb38ccd51a55d46db69f7ab7f328d0c431ca250
Marek Hanzal <marek-hanzal@edde-framework.org>
2017-03-14 19:46:05 +0100

    - fixed inner foreach

--- src/Edde/Common/Template/Macro/InnerForeachMacro.php
+++ src/Edde/Common/Template/Macro/InnerForeachMacro.php
@@ -6,20 +6,25 @@
 	use Edde\Api\Node\INode;
 	use Edde\Api\Template\IMacro;
 	use Edde\Api\Template\ITemplate;
-	use Edde\Common\Strings\StringUtils;
 	use Edde\Common\Template\AbstractMacro;
 
 	class InnerForeachMacro extends AbstractMacro {
 		/**
+		 * @var int
+		 */
+		protected $foreach = 1;
+
+		/**
 		 * @inheritdoc
 		 */
 		public function inline(IMacro $source, ITemplate $template, \Iterator $iterator, INode $node, $value = null) {
-			$value = StringUtils::toCamelHump($value);
 			$source->on(self::EVENT_POST_ENTER, function () use ($value) {
-				echo '<?php foreach($context[null]->' . $value . " as \$a => \$b) {?>";
+				echo '<?php foreach(' . $this->delimite($value) . ' as $' . str_repeat('i', $this->foreach) . ' => $' . str_repeat('j', $this->foreach) . ') {?>' . "\n";
+				$this->foreach++;
 			});
 			$source->on(self::EVENT_PRE_LEAVE, function () use ($value) {
 				echo "<?php } ?>\n";
+				$this->foreach--;
 			});
 		}
 	}
