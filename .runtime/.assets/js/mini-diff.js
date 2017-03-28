653f76fb7c0bba50dd790cffa2b778ec307e9690
Marek Hanzal <marek-hanzal@edde-framework.org>
2017-03-28 12:30:26 +0200

    - updated idea of diff protocol

--- .runtime/.assets/js/mini-diff.js
+++ .runtime/.assets/js/mini-diff.js
@@ -4,16 +4,45 @@
  */
 var MiniDiff = {
 	Commands: {
-		'add-node': function () {
-			console.log('node');
-			console.log(arguments);
+		'current': null,
+		'add-node': function (command) {
+			var root = command.path ? Sizzle(command.path)[0] : this.current;
+			var node = this.current = document.createElement(command.node);
+			if (command.text) {
+				node.appendChild(document.createTextNode(command.text));
+			}
+			root.appendChild(node);
+		},
+		'set-text': function (command) {
+			var root = command.path ? Sizzle(command.path)[0] : this.current;
+			root.innerText = command.text;
+		},
+		'remove-node': function (command) {
+			var root = command.path ? Sizzle(command.path)[0] : this.current;
+			root.parentNode.removeChild(root);
+		},
+		'add-class': function (command) {
+			var root = command.path ? Sizzle(command.path)[0] : this.current;
+			var nameList = command.class.split(' ');
+			var length = nameList.length;
+			for (var i = 0; i < length; i++) {
+				root.classList.add(nameList[i]);
+			}
+		},
+		'remove-class': function (command) {
+			var root = command.path ? Sizzle(command.path)[0] : this.current;
+			var nameList = command.class.split(' ');
+			var length = nameList.length;
+			for (var i = 0; i < length; i++) {
+				root.classList.remove(nameList[i]);
+			}
 		}
 	},
 	patch: function (commandList, root) {
 		var length = commandList.length;
 		for (var i = 0; i < length; i++) {
 			command = commandList[i];
-			this['Commands'][command['command']].apply(this, command['parameter-list'])
+			this['Commands'][command['command']].call(this, command);
 		}
 	}
 };
