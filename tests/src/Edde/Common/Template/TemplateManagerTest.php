ba169a519d85a06a36002ddc8d3d79c16835969b
Marek Hanzal <marek-hanzal@edde-framework.org>
2017-04-24 07:58:08 +0200

    - obsolete commented code is not neccesarry

--- tests/src/Edde/Common/Template/TemplateManagerTest.php
+++ tests/src/Edde/Common/Template/TemplateManagerTest.php
@@ -45,25 +45,6 @@
 			$template->execute();
 		}
 
-		// public function testException2() {
-		// 	$this->expectException(UnknownTemplateException::class);
-		// 	$this->expectExceptionMessage('Requested template name [foo-bar] cannot be found.');
-		// 	$this->templateManager->registerTemplateProvider($this->container->create(DirectoryTemplateProvider::class, [$this->rootDirectory]));
-		// 	$this->templateManager->template('foo-bar');
-		// }
-		//
-		// public function testTemplate() {
-		// 	$this->templateManager->registerTemplateProvider($this->container->create(DirectoryTemplateProvider::class, [$this->rootDirectory]));
-		// 	$template = $this->templateManager->template([
-		// 		'here-is-hidden-content-of-the-fucking-template',
-		// 		'layout',
-		// 	],);
-		// 	$template->execute([
-		// 		null      => $context = new \SomeTemplateContext(),
-		// 		'doo-bar' => $context,
-		// 	]);
-		// }
-
 		protected function setUp() {
 			$this->container = ContainerFactory::container([
 				IRootDirectory::class     => ContainerFactory::instance(RootDirectory::class, [__DIR__]),
