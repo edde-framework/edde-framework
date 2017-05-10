402d130758eec45a3c2ac095770f247ca6c5526e
Marek Hanzal <marek-hanzal@edde-framework.org>
2017-05-10 11:06:12 +0200

    - preparing REST service for usage with Protocol\ProtocolService

--- src/Edde/Ext/Rest/ProtocolService.php
+++ src/Edde/Ext/Rest/ProtocolService.php
@@ -3,16 +3,26 @@
 
 	namespace Edde\Ext\Rest;
 
+	use Edde\Api\Protocol\LazyProtocolServiceTrait;
 	use Edde\Api\Url\IUrl;
 	use Edde\Common\Rest\AbstractService;
 	use Edde\Ext\Application\JsonResponse;
 
 	class ProtocolService extends AbstractService {
+		use LazyProtocolServiceTrait;
+		protected $action;
+		protected $id;
+
 		/**
 		 * @inheritdoc
 		 */
 		public function match(IUrl $url): bool {
-			return $url->match('~^/api/v1/protocol$~') !== null;
+			if (($match = $url->match('~^/api/v1/protocol(/(?<action>.+?)(/(?<id>.+?))?)?$~')) === null) {
+				return false;
+			}
+			$this->action = $match['action'] ?? null;
+			$this->id = $match['id'] ?? null;
+			return true;
 		}
 
 		/**
@@ -20,6 +30,16 @@
 		 */
 		public function link($generate, ...$parameterList) {
 			return parent::link('/api/v1/protocol', ...$parameterList);
+		}
+
+		public function restGet(string $scope = null) {
+			switch ($this->action) {
+				case 'reference':
+					// $this->protocolService->reference();
+					dump('give the reference of ' . $this->id);
+					break;
+			}
+			return new JsonResponse([]);
 		}
 
 		public function restPost() {
