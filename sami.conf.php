6f4ff2e1223cdce0b4672036c50535ca6008abe7
Marek Hanzal <marek-hanzal@edde-framework.org>
2017-05-14 15:59:49 +0200

    - another doc trial

---
+++ sami.conf.php
@@ -0,0 +1,11 @@
+<?php
+	use Sami\Sami;
+	use Sami\Version\GitVersionCollection;
+
+	return new Sami($src = (__DIR__ . '/src'), [
+		'title'                => 'Edde Framework',
+		'versions'             => GitVersionCollection::create($src)->addFromTags('*')->add('*'),
+		'build_dir'            => __DIR__ . '/doc/%version%',
+		'cache_dir'            => __DIR__ . '/temp/doc/cache/%version%',
+		'default_opened_level' => 2,
+	]);
