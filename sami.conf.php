0788d3b5cd4fcfce0ad2611f0d00a515b0be8085
Marek Hanzal <marek-hanzal@edde-framework.org>
2017-05-14 16:07:50 +0200

    - updated dying sami

--- sami.conf.php
+++ sami.conf.php
@@ -4,7 +4,7 @@
 
 	return new Sami($src = (__DIR__ . '/src'), [
 		'title'                => 'Edde Framework',
-		'versions'             => GitVersionCollection::create($src)->addFromTags('*')->add('*'),
+		//'versions'             => GitVersionCollection::create($src)->addFromTags('*')->add('*'),
 		'build_dir'            => __DIR__ . '/doc/%version%',
 		'cache_dir'            => __DIR__ . '/temp/doc/cache/%version%',
 		'default_opened_level' => 2,
