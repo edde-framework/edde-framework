3299be6628416290114e2cc56b4c38b30a60cdbd
Marek Hanzal <marek-hanzal@edde-framework.org>
2017-06-21 18:06:02 +0200

    - improving rest api for protocol

--- src/Edde/Ext/Rest/ProtocolService.php
+++ src/Edde/Ext/Rest/ProtocolService.php
@@ -7,15 +7,19 @@
 	use Edde\Api\Protocol\IElement;
 	use Edde\Api\Protocol\LazyElementStoreTrait;
 	use Edde\Api\Protocol\LazyProtocolManagerTrait;
+	use Edde\Api\Store\LazyStoreManagerTrait;
 	use Edde\Api\Thread\LazyThreadManagerTrait;
 	use Edde\Api\Url\IUrl;
 	use Edde\Common\Protocol\Error;
 	use Edde\Common\Rest\AbstractService;
+	use Edde\Common\Store\FileStore;
 	use Edde\Ext\Protocol\ElementContent;
+	use Edde\Ext\Session\SessionStore;
 
 	class ProtocolService extends AbstractService {
 		use LazyProtocolManagerTrait;
 		use LazyElementStoreTrait;
+		use LazyStoreManagerTrait;
 		use LazyThreadManagerTrait;
 
 		/**
@@ -40,18 +44,23 @@
 		 * @return IContent
 		 */
 		public function actionGet(IElement $element) {
-			$this->response($response = new ElementContent($packet = $this->protocolManager->createPacket()));
-			if (($elementId = $element->getMeta('element'))) {
-				$reference = new Error(-101, sprintf('Requested element [%s] was not found.', $elementId));
-				if ($this->elementStore->has($elementId)) {
-					$reference = $this->elementStore->load($elementId);
+			$this->element($element);
+			try {
+				$this->response($response = new ElementContent($packet = $this->protocolManager->createPacket()));
+				if (($elementId = $element->getMeta('element'))) {
+					$reference = new Error(-101, sprintf('Requested element [%s] was not found.', $elementId));
+					if ($this->elementStore->has($elementId)) {
+						$reference = $this->elementStore->load($elementId);
+					}
+					$packet->reference($reference);
+				} else if (($referenceId = $element->getMeta('reference'))) {
+					$packet->references(iterator_to_array($this->elementStore->getReferenceListBy($referenceId)));
 				}
-				$packet->reference($reference);
-			} else if (($referenceId = $element->getMeta('reference'))) {
-				$packet->references(iterator_to_array($this->elementStore->getReferenceListBy($referenceId)));
+				$this->elementStore->save($packet);
+				return $response;
+			} finally {
+				$this->storeManager->restore();
 			}
-			$this->elementStore->save($packet);
-			return $response;
 		}
 
 		/**
@@ -62,8 +71,35 @@
 		 * @return IContent
 		 */
 		public function actionPost(IElement $element) {
-			$this->response($response = new ElementContent($this->protocolManager->execute($this->getContent($element, IElement::class))));
-			$this->threadManager->execute();
-			return $response;
+			/**
+			 * same stuff for other supported methods
+			 */
+			$this->element($element);
+			try {
+				/**
+				 * execute incoming packet and return result content
+				 */
+				$this->response($response = new ElementContent($this->protocolManager->execute($this->getContent($element, IElement::class))));
+				/**
+				 * execute thread if it is needed (are there some jobs?)
+				 */
+				$this->threadManager->execute();
+				return $response;
+			} finally {
+				/**
+				 * this is necessary to do to be safe and do not change global context
+				 */
+				$this->storeManager->restore();
+			}
+		}
+
+		protected function element(IElement $element) {
+			/**
+			 * select proper store (session/broadcast)
+			 */
+			$this->storeManager->select(SessionStore::class);
+			if ($element->getMeta('broadcast', false)) {
+				$this->storeManager->select(FileStore::class);
+			}
 		}
 	}
