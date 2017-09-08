a65027c073cd7b7fdf7c6848e3a41dc78b50d6a8
Marek Hanzal <marek-hanzal@edde-framework.org>
2017-09-08 11:50:23 +0200

    - updated application to be able to get a code from outside

--- src/Edde/Api/Application/IApplication.php
+++ src/Edde/Api/Application/IApplication.php
@@ -3,6 +3,19 @@
 
 	namespace Edde\Api\Application;
 
+	/**
+	 * Application should connect services for user input translation to
+	 * a response, for example router service to protocol service.
+	 */
 	interface IApplication {
+		/**
+		 * set an exit code from an application
+		 *
+		 * @param int $code
+		 *
+		 * @return IApplication
+		 */
+		public function setCode(int $code): IApplication;
+
 		public function run(): int;
 	}
--- src/Edde/Common/Application/Application.php
+++ src/Edde/Common/Application/Application.php
@@ -5,7 +5,6 @@
 
 	use Edde\Api\Application\IApplication;
 	use Edde\Api\Log\LazyLogServiceTrait;
-	use Edde\Api\Protocol\IElement;
 	use Edde\Api\Protocol\LazyProtocolServiceTrait;
 	use Edde\Api\Router\LazyRequestTrait;
 	use Edde\Common\Object\Object;
@@ -14,21 +13,41 @@
 		use LazyProtocolServiceTrait;
 		use LazyRequestTrait;
 		use LazyLogServiceTrait;
+		/**
+		 * return code from an application
+		 *
+		 * @var int
+		 */
+		protected $code;
 
+		/**
+		 * @inheritdoc
+		 */
+		public function setCode(int $code): IApplication {
+			$this->code = $code;
+			return $this;
+		}
+
+		/**
+		 * @inheritdoc
+		 */
 		public function run(): int {
 			try {
-				/** @var $response IElement */
-				if (($response = $this->protocolService->execute($this->request->getElement())) instanceof IElement) {
-					return (int)$response->getMeta('code', 0);
-				}
-				return 0;
+				/**
+				 * nothing special - protocol service is connected to service responsible for
+				 * providing a request
+				 */
+				$this->protocolService->execute($this->request->getElement());
+				return $this->code ?: 0;
 			} catch (\Throwable $exception) {
 				$this->logService->exception($exception, ['edde']);
 				/**
+				 * if somebody already set a code, respect it or try to guess one
+				 *
 				 * the code could be 0; so change it to something else to keep track of an
 				 * error state of an application
 				 */
-				return ($code = $exception->getCode()) === 0 ? -1 : $code;
+				return $this->code ?: (($code = $exception->getCode()) === 0 ? -1 : $code);
 			}
 		}
 	}
